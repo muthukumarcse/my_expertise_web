488d35511a9cee8a6930b84fa4e9777cb1342e15
muthukumarcse <muthukumar.paramasivam@gmail.com>
2015-10-18 18:49:30 +0530

    remote http request from angular service test

--- expertise_admin/static/js/authenticate.js
+++ expertise_admin/static/js/authenticate.js
@@ -16,11 +16,20 @@ $(document).ready(function() {
     }
     );
 
+// Create a new angular module -- authenticate
+// Create a new controller -- authenticationController and inject authenticateService to it.
+var app = angular.module('authenticate', [])
 
-angular.module('authenticate', [])
-.controller('authenticationController', ['$scope', function($scope) {
-$scope.login = function() {
-alert($scope.userName);
-}
-}
-])
+app.controller('authenticationController', ['$scope','authenticateService', function($scope, authenticateService) {
+	$scope.login = function() {
+	authenticateService.getUsers();
+
+}}])
+
+// service which will make a remote http request and access the data.
+app.service('authenticateService',['$http', function($http) {
+	var url = 'http://localhost:8081/myexpertise_frontend/users'
+	this.getUsers = function() {
+	return $http.get(url);
+};
+}])
