1fa4476cd5f35ac1dce1e1914ea90828b810095d
muthukumarcse <muthukumar.paramasivam@gmail.com>
2015-10-20 07:06:32 +0530

    User registration API call + form validation for email id

--- expertise_admin/static/js/authenticate.js
+++ expertise_admin/static/js/authenticate.js
@@ -33,3 +33,29 @@ app.service('authenticateService',['$http', function($http) {
 	return $http.get(url);
 };
 }])
+
+app.controller('registrationController', ['$scope', 'registrationService', function($scope, registrationService) {
+	$scope.register = function(user) {
+	// Invoke registration service to register a new user
+	registrationService.register(user);
+}
+}])
+
+app.service('registrationService', ['$http', function($http) {
+	this.register = function(user) {
+	var registrationReq = {
+		url: 'http://localhost:8081/myexpertise_frontend/user/register',
+		method: "POST",
+		data: { 'name' : user.name, 'email': user.email, 'phone': user.phone, 'password':user.password, 'companyId':user.companyId },
+		headers: {'Content-Type': 'application/json'}
+	    }
+	    
+	   $http(registrationReq).then(function(response) {
+
+	    }, 
+	    function(response) { // optional
+
+	    });
+};
+
+}])
